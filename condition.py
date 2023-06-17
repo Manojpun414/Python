e987316cf54009c22e274bdda13212faaa049a7e
Manojpun414 <121884004+Manojpun414@users.noreply.github.com>
2023-06-17 09:21:26 +0545

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,110 @@
+# x=10
+# y=20
+# print(x>y)
+
+# x=40
+# y=20
+# if x>y:
+#     print("x is greater than y")
+# else:
+#     print("x is less than y")
+
+# x=10
+# y=20
+# z=30
+# if x>y and x>z:
+#     print("x is greater")
+# elif y>x and y>z:
+#     print("y is greater")
+# else:
+#     print("z is greater")
+
+# age=int(input("Enter your age:"))
+# if age<=18:
+#     print(f"You are not elligible to vote {age}") --> this is the best practise
+# elif age>40:
+#     print("You are not elligible {}".format(age))
+# else:
+#     print("You are elligible",age)
+
+#WAP to enter five subject marks and calculate total,percentage,and print the grade
+# nep=float(input("Enter the marks of nepali:"))
+# phy=float(input("Enter the marks of physics:"))
+# chem=float(input("Enter the marks of chemistry:"))
+# comp=float(input("Enter the marks of computer:"))
+# math=float(input("Enter the marks of maths:"))
+# total=nep+phy+chem+comp+math
+# percentage=total/5
+# print(f"The total is: {total}")
+# print(f"The percentage is: {percentage}")
+# if percentage <=35:
+#     print("E grade")
+# elif percentage <=45:
+#     print("D grade")
+# elif percentage <=55:
+#     print("C grade")
+# elif percentage <=65:
+#     print("B grade")
+# elif percentage <=75:
+#     print("A grade")
+# elif percentage <=85:
+#     print("A+ grade")
+# else:
+#     print("Invalid percentage")
+
+#WAP to enter a number is even or odd
+# num=int(input("Enter a number:"))
+# if num %2==0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+#WAP to enter a number is even or odd
+# num=input("Enter a number:")
+# if int(num) %2==0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+#WAP to enter any number to check whether it is divisible by 5 and 3
+# num=int(input("Enter any number:"))
+# if num%5==0 and num%3==0:
+#     print("Divisible")
+# else:
+#     print("NOt Divisible")
+
+# username="admin"
+# password="admin123"
+# if username=="admin":
+#     if password=="admin123":
+#         print("Welcome to the system")
+#     else:
+#         print("Password is incorrect")
+# else:
+#     print("Username is incorrect")
+
+# x=20
+# y=30
+# z=40
+# if x>y:
+#     if x>z:
+#         print(f"{x}\n{z}\n{y}")
+#     else:
+#         print(f"{z}\n{x}\n{y}")
+# else:
+#     if y>z:
+#         print(f"{y}\n{z}\n{x}")
+#     else:
+#         print(f"{z}\n{y}\n{x}")
+
+# def main():
+#     x=int(input("Enter the number:"))
+#     if is_even(x):
+#         print("Even")
+#     else:
+#         print("Odd")
+
+# def is_even(n):
+#     return True if n%2==0 else False    
+
+
